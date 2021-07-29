8196931bf0b627ee0b28e0c176763e85fd70327f
Mike Short <mikeshort2014@gmail.com>
2021-07-29 00:14:17 -0400

    build: added apollo and codegen command

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import "../public/index.css";
 import React from "react";
 import type { AppProps /* , AppContext */ } from "next/app";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const client = new QueryClient();
+  const client = new ApolloClient({
+    uri: "https://qhbu2ucdjnhfthvuco4swnhdb4.appsync-api.us-east-1.amazonaws.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <QueryClientProvider client={client}>
+    <ApolloProvider client={client}>
       <Component {...pageProps} />
-    </QueryClientProvider>
+    </ApolloProvider>
   );
 }
 
